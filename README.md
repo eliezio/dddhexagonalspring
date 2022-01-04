763e18836b698976575b65b8636aa43a90bd00a3
Eliezio Oliveira <eliezio@pm.me>
2022-01-04 16:17:23 +0000

    chore(build): initial commit

---
+++ README.md
@@ -0,0 +1,23 @@
+This project was derived from:
+https://github.com/eugenp/tutorials/tree/master/ddd/src/main/java/com/baeldung/dddhexagonalspring
+
+See the original companion article:
+
+- [Organizing Layers Using Hexagonal Architecture, DDD, and Spring](https://www.baeldung.com/hexagonal-architecture-ddd-spring)
+
+## How to build
+
+### Step 1: Launch required services
+
+```console
+cd src/test/resources
+docker-compose up
+```
+
+### Step 2: Build and run tests
+
+On a separated terminal window, go to project's home directory and run:
+
+```console
+./gradlew build
+```
---
+++ build.gradle.kts
@@ -0,0 +1,38 @@
+plugins {
+    application
+
+    id("org.springframework.boot") version "2.6.2"
+    id("com.adarshr.test-logger") version "3.1.0"
+}
+
+apply(plugin = "io.spring.dependency-management")
+
+description = "DDD series examples"
+group = "com.baeldung"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_11
+}
+
+application {
+    mainClass.set("com.baeldung.dddhexagonalspring.DomainLayerApplication")
+}
+
+repositories {
+    mavenCentral()
+}
+
+dependencies {
+    implementation("org.springframework.boot", "spring-boot-starter")
+    implementation("org.springframework.boot", "spring-boot-starter-web")
+    implementation("org.springframework.boot", "spring-boot-starter-validation")
+    implementation("org.springframework.boot", "spring-boot-starter-data-mongodb")
+    implementation("org.springframework.boot", "spring-boot-starter-data-cassandra")
+
+    testImplementation("org.springframework.boot", "spring-boot-starter-test")
+}
+
+tasks.test {
+    useJUnitPlatform()
+}
